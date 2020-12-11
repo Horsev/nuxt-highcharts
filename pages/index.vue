0b12ec83b5137dd08cb9d31393e5252a25e0321a
Horsev <36910890+Horsev@users.noreply.github.com>
2020-12-10 23:39:57 -0500

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+section
+  h1 Hello Highcharts
+  highcharts(:options="chartOptions")
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      chartOptions: {
+        series: [
+          {
+            data: [1, 2, 3]
+          }
+        ]
+      }
+    }
+  }
+}
+</script>
